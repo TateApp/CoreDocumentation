cdc0fe1350180a27f777d7fa79ef60cbae49bc5f
Tate <99022828+TateApp@users.noreply.github.com>
2022-04-28 21:37:05 +0100

    Add files via upload

---
+++ CoreDocumentation/CoreDocumentation/CoreDocumentation.h
@@ -0,0 +1,18 @@
+//
+//  CoreDocumentation.h
+//  CoreDocumentation
+//
+//  Created by Tate Wrigley on 28/04/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreDocumentation.
+FOUNDATION_EXPORT double CoreDocumentationVersionNumber;
+
+//! Project version string for CoreDocumentation.
+FOUNDATION_EXPORT const unsigned char CoreDocumentationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDocumentation/PublicHeader.h>
+
+
